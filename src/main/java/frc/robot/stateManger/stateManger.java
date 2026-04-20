b9b417c33453426dded89a9ac454d387c8b604ec
Matan Dizengof <diz.matan@gmail.com>
2026-04-20 22:31:35 +0300

    fix error i duom

--- src/main/java/frc/robot/stateManger/stateManger.java
+++ src/main/java/frc/robot/stateManger/stateManger.java
@@ -54,12 +54,12 @@ public class stateManger extends SubsystemBase{
                 }
         }else{
             RobotContainer.shooter.setShooterState(ShooterStates.TRANCH);
-        }else{
-            RobotContainer.shooter.setShooterState(ShooterStates.IDLE);
-            RobotContainer.turret.setState(TurretStates.IDLE);
-            RobotContainer.intake.setState(IntakeState.IDLE);
         }
-    }  
+    }else{
+        RobotContainer.shooter.setShooterState(ShooterStates.IDLE);
+        RobotContainer.turret.setState(TurretStates.IDLE);
+        RobotContainer.intake.setState(IntakeState.IDLE);
+    }
 }
 
 }
