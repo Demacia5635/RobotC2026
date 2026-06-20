df18c02792276e03a19f3096ae54945eac18f881
matan <diz.matan@mail.com>
2026-06-20 15:18:05 +0300

    Revert "(idan)  testing intake"
    This reverts commit e019f769a3de9dc30c199a6c93562ee663164501.

--- src/main/java/frc/robot/shinua/ShinuaConstants.java
+++ src/main/java/frc/robot/shinua/ShinuaConstants.java
@@ -52,8 +52,11 @@ public class ShinuaConstants {
     public static enum ShinuaState {
         SHINUA_ON(-12.5, 1),
         SHINUA_OFF(0, 0),
-        EJECTING(-12.5, -1),
+        EJECTING(-12.5, 1),
+<<<<<<< HEAD
+=======
         NO_INDEXER(-12.5 , 0),
+>>>>>>> 60f44d4d3aca39cfa70e9188f4380e4b12eddf1b
         TESTING(0, 0);
 
         public double velocityRollers;
